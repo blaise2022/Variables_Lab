797efc9d68946862b85e596e1f1ca0440439ea96
Blaise2022 <blaisegm31@gmail.com>
2023-09-11 17:52:58 -0500

    First

---
+++ src/Variables.java
@@ -0,0 +1,44 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+
+        byte variable1 = 19;
+        short variable2 =  31000;
+        int variable3 = 3874;
+        long variable4 = 8978978;
+        float variable5 = 2495.87F;
+        double variable6 = 896.54;
+        char variable7 = 'B';
+        boolean variable8 = true;
+        String variable9 = "Happy Birthday";
+
+        System.out.println("/////////////////////////////////////////////////");
+        System.out.println("* Blaise Martinez                      09/11/23 *");
+        System.out.println("*                                               *");
+        System.out.println("*                   Integer Types               *");
+        System.out.println("*                                               *");
+        System.out.println("* 8 bit - byte_variable = " + variable1 + "                    *");
+        System.out.println("* 16 bit - short_variable = " + variable2 + "               *");
+        System.out.println("* 32 bit - int_variable = " + variable3 + "                  *");
+        System.out.println("* 64 bit - long_variable = " +variable4 + "              *");
+        System.out.println("*                                               *");
+        System.out.println("*                   Real Types                  *");
+        System.out.println("*                                               *");
+        System.out.println("* 32 bit - float_variable = " + variable5 + "             *");
+        System.out.println("* 64 bit - double_variable = " + variable6 + "             *");
+        System.out.println("*                                               *");
+        System.out.println("*            Other Integer Types                *");
+        System.out.println("*                                               *");
+        System.out.println("* 16 bit - char_variable = " + variable7 + "                    *");
+        System.out.println("*                                               *");
+        System.out.println("*                 Other Types                   *");
+        System.out.println("*                                               *");
+        System.out.println("* only_boolean = " + variable8 + "                           *");
+        System.out.println("* only_string = " + variable9 + "                  *");
+        System.out.println("/////////////////////////////////////////////////");
+
+    }
+
+
+}
